a6b32c3c8944558536fa60352f7831bf828e6070
douksieh-X441BA <douksieha@gmail.com>
2021-03-02 10:37:24 +0100

    creation des tables Choice et Question

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,10 @@
 #from django.shortcuts import render
 from django.http import HttpResponse
+import datetime
+
+datedujour = datetime.datetime.now().strftime('%Y-%m-%d  %H:%M:%S')
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Bonjour tous le monde, La page d'accueil de l'appli des sondages.")
+    return HttpResponse("Bonjour tous le monde, La page d'accueil de l'appli des sondages ({})".format(datedujour))
 
